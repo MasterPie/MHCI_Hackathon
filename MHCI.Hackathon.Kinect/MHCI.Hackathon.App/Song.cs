1a21402b8e36dc340c9a35b08e35562d563ef01e
Kaitlyn Sparks <kaitlynesparks@gmail.com>
2014-12-06 01:36:08 -0500

    music sucks

---
+++ MHCI.Hackathon.Kinect/MHCI.Hackathon.App/Song.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHCI.Hackathon.App.Model
+{
+    class Song
+    {
+        public Song(string name, string fileLocation)
+        {
+            this._name = name;
+            this._fileLocation = fileLocation;
+        } 
+        private string _fileLocation;
+        public string FileLocation
+        {
+            get
+            {
+                return _fileLocation;
+            }
+            set
+            {
+                _fileLocation = value;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
