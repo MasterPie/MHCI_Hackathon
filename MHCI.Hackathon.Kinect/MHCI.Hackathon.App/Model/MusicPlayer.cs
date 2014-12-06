d686dac17d600ac509f6f16ce8c60f16a07f3aa9
KaitlynSparks <kaitlynesparks@gmail.com>
2014-12-05 21:53:50 -0500

    Add irrKlang library and classes

---
+++ MHCI.Hackathon.Kinect/MHCI.Hackathon.App/Model/MusicPlayer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IrrKlang;
+
+namespace MHCI.Hackathon.App.Model
+{
+    class MusicPlayer
+    { 
+        private ISoundEngine _engine;
+        private ISound _currentlyPlayingSound;
+
+        public MusicPlayer()
+        {
+            this._engine = new ISoundEngine();
+        }
+
+        public void Play(Song song)
+        {
+            try
+            {
+                this.Stop();
+                this._currentlyPlayingSound = this._engine.Play2D(song.FileLocation, true);
+                
+                if (this._currentlyPlayingSound == null)
+                {
+                    throw new ArgumentException("Unable to play song");
+                }
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void Pause()
+        {
+            if (IsPlaying)
+            {
+                this._currentlyPlayingSound.Paused = true;
+            }
+        }
+        public void Stop()
+        {
+            if(HasSoundFile)
+            {
+                this._currentlyPlayingSound.Stop();
+            }
+        }
+
+        public bool HasSoundFile
+        {
+            get
+            {
+                return this._currentlyPlayingSound != null && !this._currentlyPlayingSound.Finished;
+            }
+        }
+
+        public bool IsPlaying
+        {
+            get
+            {
+                return HasSoundFile && !this._currentlyPlayingSound.Paused;
+            }
+        }
+
+    }
+}
---
+++ MHCI.Hackathon.Kinect/MHCI.Hackathon.App/Model/Song.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHCI.Hackathon.App.Model
+{
+    class Song
+    {
+        private string _fileLocation;
+        public string FileLocation
+        {
+            get
+            {
+                return _fileLocation;
+            }
+            set
+            {
+                _fileLocation = value;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
