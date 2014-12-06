a94f8fc3ccd7ac9fd5540dfd508f351fea65e7e7
Vivek Pai <VivekPai@live.com>
2014-12-05 21:54:58 -0500

    Making progress with getting better ids.

---
+++ MHCI.Hackathon.Kinect/MHCI.Hackathon.App/Kinect/AutomatedInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MHCI.Hackathon.App.Kinect
+{
+    public class AutomatedInput : IPlayerInput
+    {
+        private Timer _timer;
+
+        public AutomatedInput()
+        {
+            this._timer = new Timer(10);
+            this._timer.Elapsed += _timer_Elapsed;
+            this._timer.Start();
+        }
+
+        void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            
+            
+        }
+
+
+        public event EventHandler<IEnumerable<Model.Action>> PlayerActionsChanged;
+        public event EventHandler<int> PlayerJoined;
+        public event EventHandler<int> PlayerLeft;
+    }
+}
